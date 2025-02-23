5310fdc4dd37d1873c4c4ddca330868ea6cf4701
devBlasquez <mauricio.junior@estudante.ufscar.br>
2025-02-23 18:36:21 -0300

    feat: created functions on car_scraper
    - function to list categorys

--- car_scraper.py
+++ car_scraper.py
@@ -1,27 +1,69 @@
 import requests
 from bs4 import BeautifulSoup
 
+#Lista as categorias sendo cada uma delas um objeto
+def linksCategory(soup):
+        links = soup.find_all(class_='category__link')
+
+        categorys = []
+
+    # Exibir os links
+        for link in links:
+            nameCategory = link.find('span', class_ = 'category__label').get_text(strip = True)
+            href = link.get('href')
+
+            category = {
+            'name': nameCategory,
+            'link': href
+            }
+            
+            categorys.append(category)
+
+        return categorys
+
+
+
+
 # URL da página
 url = 'https://www.carpages.ca/'
 
+url_cars = 'https://www.guiadoautomovel.pt/marcashttps://www.guiadoautomovel.pt/marcas'
+
 # Fazer a requisição HTTP
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36'}
-response = requests.get(url, headers=headers)
+
+#Acesso a página principal
+responseMain = requests.get(url, headers=headers)
+
+#Acesso as marcas de carros
+responseCarsList = requests.get(url_cars, headers=headers)
+
+#Lista de marcas de carros
+
+#Verificar se a requisição foi bem-sucedida
+if(responseCarsList.status_code == 200):
+    soupCars = BeautifulSoup(responseCarsList.content, 'html.parser')
+    carsMarks = soupCars.find('div', class_ = 'todas-marcas')
+    carMark = carsMarks.find('div', class_ = 'cada-marca').find('h2')
+    # carMarks = carsTag.get_text(strip = True)
+    print(carMark)
+
+else
+    print(f"Erro ao acessar a página: {responseCarsList.status_code}")
 
 # Verificar se a requisição foi bem-sucedida
-if response.status_code == 200:
-    soup = BeautifulSoup(response.content, 'html.parser')
+if responseMain.status_code == 200:
+    soupMain = BeautifulSoup(responseMain.content, 'html.parser')
 
-    # Encontrar todas as tags <a>
-    links = soup.find_all(class_='category__link')
+    linksCategory = linksCategory(soupMain) 
 
-    # Criar uma lista com os href dos links
-    hrefs = [link.get('href') for link in links if link.get('href')]
+    for item in linksCategory:
+        pageForCategory = BeautifulSoup(requests.get(item['link'], headers=headers).content, 'html.parser')
 
-    # Exibir os links
-    for href in hrefs:
-        soup_body_style = BeautifulSoup(requests.get(href).content, 'html.parser')
-        print(soup_body_style)
+
+    
 
 else:
-    print(f"Erro ao acessar a página: {response.status_code}")
+    print(f"Erro ao acessar a página: {responseMain.status_code}")
+
+
