f77a9de2ea0e2eafe78777f316b14108231d6eb9
devBlasquez <mauricio.junior@estudante.ufscar.br>
2025-02-24 01:59:14 -0300

    feat: create functions
    - create function linksCategory: function to insert liks from categorys on a dictionary
    
    - create netxPage: function to go to the next page
    
    - create accessPageCars: function to access page of car and get the description

--- car_scraper.py
+++ car_scraper.py
@@ -1,5 +1,63 @@
+import time
 import requests
 from bs4 import BeautifulSoup
+
+# URL da página
+base_url = 'https://www.carpages.ca/'
+
+# url_cars = 'https://www.guiadoautomovel.pt/marcashttps://www.guiadoautomovel.pt/marcas'
+
+# Fazer a requisição HTTP
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36'}
+
+#Adiciona Kilometragem a lista de carros
+def addMileage(listCars):
+    
+    for listCar in listCars:
+        link = listCar['namelink']
+
+        time.sleep(2)
+        itemLink = BeautifulSoup(requests.get(base_url.rstrip('/') + '/' + link.lstrip('/'), headers=headers).content, 'html.parser')
+
+
+        mileage = itemLink.find('span')
+
+        print(mileage)
+
+
+#Lista os carros em uma página
+def accessPageCars (soup):
+    
+    cars = soup.find_all(class_ = 't-flex t-gap-6 t-items-start t-p-6')
+
+    listCars = []
+
+    for car in cars:
+        nameCar = car.find('h4', class_ = 'hN').find('a').get('title')
+        descriptionCar = car.find('h5').get_text(strip=True)
+        link =  car.find('h4', class_ = 'hN').find('a').get('href')
+
+        car = {
+            'name': nameCar,
+            'description': descriptionCar,
+            'namelink': link,
+            'mileage': ''
+        }
+
+        listCars.append(car)
+    
+    return listCars
+
+def nextPage(soup):
+    #Botao da próxima página
+    nextPages = soup.find('a', {'title':'Next Page'})
+    #Checagem se há próxima página
+    if not nextPages:
+        return None
+
+    next_url = nextPages.get('href')
+
+    return base_url.rstrip('/') + '/' + next_url.lstrip('/')
 
 #Lista as categorias sendo cada uma delas um objeto
 def linksCategory(soup):
@@ -24,32 +82,25 @@ def linksCategory(soup):
 
 
 
-# URL da página
-url = 'https://www.carpages.ca/'
 
-url_cars = 'https://www.guiadoautomovel.pt/marcashttps://www.guiadoautomovel.pt/marcas'
-
-# Fazer a requisição HTTP
-headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36'}
 
 #Acesso a página principal
-responseMain = requests.get(url, headers=headers)
+responseMain = requests.get(base_url, headers=headers)
 
 #Acesso as marcas de carros
-responseCarsList = requests.get(url_cars, headers=headers)
+# responseCarsList = requests.get(url_cars, headers=headers)
 
 #Lista de marcas de carros
 
 #Verificar se a requisição foi bem-sucedida
-if(responseCarsList.status_code == 200):
-    soupCars = BeautifulSoup(responseCarsList.content, 'html.parser')
-    carsMarks = soupCars.find('div', class_ = 'todas-marcas')
-    carMark = carsMarks.find('div', class_ = 'cada-marca').find('h2')
-    # carMarks = carsTag.get_text(strip = True)
-    print(carMark)
+# if(responseCarsList.status_code == 200):
+#     soupCars = BeautifulSoup(responseCarsList.content, 'html.parser')
+#     carMark = soupCars.find('div', class_ = 'cada-marca').find('h2').get_text(strip=True)
+#     print(carMark)
 
-else
-    print(f"Erro ao acessar a página: {responseCarsList.status_code}")
+# else:
+#     print(f"Erro ao acessar a página: {responseCarsList.status_code}")
+
 
 # Verificar se a requisição foi bem-sucedida
 if responseMain.status_code == 200:
@@ -58,10 +109,32 @@ if responseMain.status_code == 200:
     linksCategory = linksCategory(soupMain) 
 
     for item in linksCategory:
-        pageForCategory = BeautifulSoup(requests.get(item['link'], headers=headers).content, 'html.parser')
+        flag = False
+
+        current_url = item['link']
+
+        
+
+        while True:
+            listCarsPages = BeautifulSoup(requests.get(current_url, headers=headers).content, 'html.parser')
 
 
-    
+            if not flag:
+                accessPageCars(listCarsPages)
+            
+            flag = True
+
+            pageForCategory = BeautifulSoup(requests.get(current_url, headers=headers).content, 'html.parser')
+
+            current_url = nextPage(pageForCategory)
+
+            if not current_url:
+                break
+            
+            listCars =  accessPageCars(listCarsPages)
+
+            addMileage(listCars)
+
 
 else:
     print(f"Erro ao acessar a página: {responseMain.status_code}")
